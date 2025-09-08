7e17c35e693aaaf9ea8faae3604400f89bafe171
Reto <reto.scheiwiller5@bluewin.ch>
2025-09-08 08:48:55 +0200

    started working on HTML Report

--- reportFiles/datafiles.js
+++
@@ -1,9 +0,0 @@
-//DATA_FILES = ["mandatoryTest.js", "data.js", "data2.js"];
-
-DATA_FILES=[];
-DATA_FILES.push("mandatoryTest.js");
-DATA_FILES.push("data.js");
-DATA_FILES.push("data.js");
-DATA_FILES.push("data.js");
-DATA_FILES.push("bigdata.js");
-DATA_FILES.push("data2.js");
